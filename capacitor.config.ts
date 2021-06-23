d3026e479168c7ee22d303eed6db874521c6da40
FugaTakata <maple1213goo@gmail.com>
2021-06-23 16:23:21 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'zettai',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
